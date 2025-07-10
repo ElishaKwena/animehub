1ce03a8d47f24f3189f179cb09bf633ba0abec1d
ElishaKwena <elishakwena@gmail.com>
2025-07-10 20:29:44 +0300

    few changes

--- src/components/PopularCarousel.tsx
+++ src/components/PopularCarousel.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper/modules';
-import 'swiper/css';
-import 'swiper/css/navigation';
 import { popularAnime } from '../data/Anime';
 import Popular from './Popular';
 
