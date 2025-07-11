f3e470bdbf313a75e24131bf360a55cb0757edff
ElishaKwena <elishakwena@gmail.com>
2025-07-11 23:14:50 +0300

    vercel.json deleted

--- frontend/src/pages/authentication/Signup.tsx
+++ frontend/src/pages/authentication/Signup.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState, useEffect} from 'react'
+import {useState} from 'react'
 import { Link } from 'react-router-dom';
 import ROUTE_PATHS from '../../routes/paths';
 import Authbackground from '../../components/MaterialUI/Background/Authbackground';
